1b680a3b4e154a15020e4de2dad56eb72cc4ee5e
Alyssa Hope <alyssa@organikseo.com>
2018-04-23 15:00:37 -0700

    initial commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,12 @@
+class MainController < ApplicationController
+
+  def answers
+    if params[:number].to_i.even?
+      @result_string = "Even"
+    else
+      @result_string = "Odd"
+    end
+    #render "answers.html.erb"
+  end
+
+end
